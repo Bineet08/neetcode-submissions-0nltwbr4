cfd73bf520fc163ae3097f2018e503fdb7c54b7d
Bineet Gupta <bineet.gupta.civ23@itbhu.ac.in>
2026-04-13 18:53:22 +0530

    Add: minimum-window-with-characters - submission-3

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-3.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        unordered_map<char,int> freq;
+        for(char c : t) freq[c]++;
+        int required = freq.size();
+        int formed = 0;
+        unordered_map<char,int> window;
+        int l = 0, r = 0;
+        int minLen = INT_MAX, start = 0;
+
+        while(r < s.size()){
+            char c = s[r];
+            window[c]++;
+
+            if(freq.count(c) && window[c] == freq[c])
+                formed++;
+
+            while(l <= r && formed == required){
+                if(r - l + 1 < minLen){
+                    minLen = r - l + 1;
+                    start = l;
+                }
+
+                char leftChar = s[l];
+                window[leftChar]--;
+
+                if(freq.count(leftChar) && window[leftChar] < freq[leftChar])
+                    formed--;
+
+                l++;
+            }
+            r++;
+        }
+
+        return minLen == INT_MAX ? "" : s.substr(start, minLen);
+    }
+};
